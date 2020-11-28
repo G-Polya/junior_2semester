92119bc8f8fecc1748c66b9e646a9b11c030bd01
G_Polya <wlskaka4@gmail.com>
2020-11-28 19:23:57 +0900

    no message

--- 소공/ejs2/app.js
+++ 소공/ejs2/app.js
@@ -6,6 +6,19 @@ var conn = db_config.init();
 var bodyParser = require('body-parser');
 var url = require("url")
 db_config.connect(conn);
+var session=require('express-session')
+var mySqlStore= require('express-mysql-session')(session)
+let router = express.Router();
+
+var options = {
+    host : 'localhost',
+    port:3306,
+    user:'root',
+    password:'',
+    database:'clerkfree'
+}
+
+var sessionStore = new mySqlStore(options)
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -13,24 +26,20 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 
-
+app.use(session({
+    secret : "kim",
+    resave:false,
+    saveUninitialized : true,
+    store : sessionStore
+}))
 
 app.get('/', function (req, res) {
-    const body = req.body
-    const inputId = body.id
-    console.log(body)
-
+    
     res.send("root")
     //res.render('login.ejs');
 });
 
-app.get('/list', function (req, res) {
-    var sql = 'SELECT * FROM BOARD';    
-    conn.query(sql, function (err, rows, fields) { 
-        if(err) console.log('query is not excuted. select fail...\n' + err);
-        else res.render('list.ejs', {list : rows});
-    });
-});
+
 app.get('/registerPage', function (req, res) {
     let duplicateMsg=""
     res.render('register.ejs',{duplicateMsg:""})
@@ -41,40 +50,12 @@ app.post('/gotoRegister', function(req,res){
     res.redirect("/registerPage");
 })
 
-app.get('/registerFunc', function(req, res){
-    let _url = req.url
-    const queryData = url.parse(_url, true).query
-    let inputId = queryData.id
-    let inputPw = queryData.pswd1
-
-    console.log(inputId,inputPw)
-})
-
-const idCheck = (id, people) => {
-    for(var i = 0; i<people.length; i++){
-        if(id == people[i].Id){
-            return false;
-        }
-        else {
-            return true;
-        }
-    }
-}
 
 app.get('/duplicateFunc', function(req, res){
 
     const userInfo = []
 
-    const already = function(userInfo, inputId) {
-        for(var i = 0 ;i < userInfo.length; i++){
-            if(userInfo[i].userid === inputId){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-    }
+
     let duplicateMsg =""
 
     let sql = "SELECT userid, userPw from account";
@@ -100,9 +81,12 @@ app.get('/duplicateFunc', function(req, res){
        // console.log(userInfo)
 
       
-        const flag = already(userInfo, inputId)
-        
-        //res.render('register.ejs', {duplicateMsg:" "})
+        let flag = userInfo.some(function(element){
+            if(element.userid === inputId) return true
+        })
+
+        //console.log("!:" + userInfo);
+
 
         if(inputId === "") {
             res.render('register.ejs', {duplicateMsg:""})
@@ -110,52 +94,57 @@ app.get('/duplicateFunc', function(req, res){
             if(flag){
                 res.render('register.ejs', {duplicateMsg:"중복되는 ID입니다."})
             } else {
-                res.render('register.ejs', {duplicateMsg:"사용해도 좋은 ID입니다"})
+                res.render('register.ejs', {duplicateMsg:"회원가입되었습니다."})
+                sql = `insert into ACCOUNT(userId, name, userPw, isManager) values (?, ?, ?,?)`
+
+                conn.query(sql,[inputId,inputName, inputPw, 0])
+        
             }  
         }
     })
 
-    sql = `insert into ACCOUNT(userId, userPw, name, isManager) values (?, ?, ?)`
-
-     //conn.query(sql,[inputId,inputPw])
-
 
 })
 
-// app.get('/login', function (req, res) {
-//     console.log(req.body)
- 
-// });
+app.get('/order', function(req,res){
+    console.log(req.session.isLogined)
+    res.render("order.ejs")
+})
+
+app.get('/review', function(req,res){
+    console.log(req.session.isLogined)
+    res.render("review.ejs")
+})
+
+app.get('/history', function(req,res){
+    console.log(req.session.isLogined)
+    res.render("history.ejs")
+})
 
 
 
-app.get('/next', function(req,res){
-    res.render("next.ejs")
+
+
+app.get('/main', function(req,res){
+    console.log(req.session.isLogined);
+    res.render("main.ejs", {user: req.session.userid})
 })
 
 app.get('/login', function(req,res) {
     let loginMsg=""
-    res.render('login.ejs', {loginMsg:loginMsg, loginFlag:false});
+    res.render('login.ejs', {loginMsg:loginMsg});
 })
 
-app.get('/loginFunc', function (req, res) {
-  let _url = req.url
-  const queryData = url.parse(_url, true).query
-  let inputId = queryData.id
-  let inputPw = queryData.pw
+app.post('/loginFunc', function (req, res) {
+
+  const body = req.body
+  const inputId = body.id
+  const inputPw = body.pw
+//  console.log(inputId,inputPw)
 
   const userInfo = []
 
-  const authenticate = function(userInfo, inputId, inputPw) {
-    for(var i = 0 ;i < userInfo.length; i++){
-        if(userInfo[i].userid === inputId && userInfo[i].userPw === inputPw){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-  }
+  
 
   const sql = "SELECT userid, userPw from account";
   conn.query(sql, function(err, rows, fields){
@@ -164,13 +153,28 @@ app.get('/loginFunc', function (req, res) {
         for(var i = 0; i < rows.length; i++){   
             userInfo.push( {userid:rows[i].userid, userPw: rows[i].userPw})
         }
-
-        const flag = authenticate(userInfo, inputId, inputPw)
+        //console.log(userInfo)
+        
+        let flag = userInfo.some(function(element){
+            if(element.userid === inputId && element.userPw === inputPw){
+                return true;
+            }
+        })
 
         if(inputId === "") {
             res.render('login.ejs', {loginMsg:""})
         } else {
-            res.render('login.ejs',{loginMsg:""})
+            if(flag){
+                req.session.userid = inputId
+                req.session.userPw = inputPw
+                req.session.isLogined = true
+
+                req.session.save(function(){
+                    res.redirect('/main')
+                })
+            } else {
+                res.render('login.ejs', {loginMsg:"등록되지 않은 ID 또는 PW입니다"})
+            }  
         }
 
     }
