c8c363c482a414bb4db03d3bac23c6f6446c4e9f
G_Polya <wlskaka4@gmail.com>
2020-12-17 19:57:41 +0900

    no message

--- 컴파일러/Ucode_Translator/Ucode_Translator/SymTab.c
+++ 컴파일러/Ucode_Translator/Ucode_Translator/SymTab.c
@@ -1,74 +1,271 @@
-#include "sdt.c"
-#define NO_KEYWORDS 7
-#define ID_LENGTH 12
-//스택사이즈.
-#define STACKSIZE 20000;
-#define NO_OPCODES 41;
-//최대 라벨 크기.
-#define LABEL_SIZE 7
-//최대 해쉬 버킷 사이즈.
-#define BUCKET_SIZE 97	
-//최대 함수 크기.
-#define MAX_LIBRARY_FUNC 3
-//연산자 크기.
-#define OPCODE_SIZE 5
-#define FIRSTWORD	11
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include "parser.h"
+#include "scanner.h"
 
+#define HASH_BUCKET_SIZE 97
+#define SYMTAB_SIZE 200
+#define LEVEL_STACK_SIZE 10
+#define LABEL_SIZE 12
 
-//연산자 이름
-const char* opcodeName[] = {
-	"notop",	"neg",	"inc",	"dec",	"dup",	"swp",	"add",	"sub",	"mult",	"div",
-	"mod",		"and",	"or",	"gt",	"lt",	"ge",	"le",	"eq",	"ne",
-	"lod",		"ldc",	"lda",	"ldi",	"ldp",	"str",	"sti",	"ujp",	"tjp",	"fjp",
-	"call",		"ret",	"retv",	"chkh",	"chkl",	"nop",	"proc",	"end",	"bgn",	"sym",
-	"dump",		"none"
+extern FILE* ucodeFile;
+
+void initSymbolTable();
+int hash(char*);
+int lookup(char*);
+int insert(char*, int, int, int, int, int, int);
+void dumpSymbolTable();
+void set();
+void reset();
+
+typedef enum {
+	NON_SPECIFIER, VOID_TYPE, INT_TYPE
+} TypeSpecifier;
+
+typedef enum {
+	NON_QUALIFIER, FUNC_TYPE, PARAM_TYPE, CONST_TYPE, VAR_TYPE
+} TypeQuailfier;
+
+char* typeName[] = {
+		"none",   "void",   "int"
 };
 
-//심볼이름 구조체, 인덱스와 길이를 가지고 있다.
-struct Table;
-typedef	struct symbol {
-	int index;
-	int length;
-} symbolName;
+char* qualifierName[] = {
+		"NONE", "FUNC_TYPE",  "PARAM_TYPE",  "CONST_TYPE", "VAR_TYPE"
+};
 
+typedef enum {
+	ZERO_DIMENSION, ONE_DIMENSION
+} Dimension;
 
-//심볼 테이블 구조체
-typedef struct Table {
-	symbolName symname;
-	int index;
+struct SymbolEntry {
+	char symbolName[ID_LENGTH];
+	int typeSpecifier;
+	int typeQualifier;
 	int base;
 	int offset;
-	int width;
-	int typeQualifier;
-	int typeSpecifier;
-	int initialValue;
-} symbolTable;
-
-
-//베이스, 오프셋, 너비
-int base = 1, offset = 1, width = 1;
-//left value, right value
-int lvalue, rvalue;
-//파싱 테이블 사이즈만한 level Table 
-int levelTable[PS_SIZE];
-//심벌테이블 인덱스 
-int stIdx = 0;
-//스트링풀 인덱스
-int stringpoolIndex = 0;
-//스트링풀 변수
-char stringPool[1000];
-//가능한 인덱스
-int AvailIndex = 0;
-int topOfLevelTable;
-//버켓 사이즈만한 해쉬 버켓
-symbolTable hashBucket[BUCKET_SIZE];
-//버켓 사이즈만한 심볼테이블 
-symbolTable symTable[BUCKET_SIZE];
-
-//리턴 타임 enumeration
-enum returnt {
-	INT_TYPE, VOID_TYPE, VAR_TYPE, CONST_TYPE,
-	FUNC_TYPE
+	int width;					// size
+	int initialValue;			// initial value
+	int nextIndex;				// link to next entry.
 };
+struct SymbolEntry symbolTable[SYMTAB_SIZE];		// symbol table
+int symbolTableTop;
+int hashBucket[HASH_BUCKET_SIZE];
+int levelStack[SYMTAB_SIZE];						// level table
+int levelTop;
+int base, offset;
 
-FILE* ucodeFile;                        // ucode file
+void emitSym(int base, int offset, int size) {
+	fprintf(ucodeFile, "           sym %d %d %d\n", base, offset, size);
+	printf("           sym %d %d %d\n", base, offset, size);
+}
+
+
+void emitJump(char* value, char* label) {
+	fprintf(ucodeFile, "           %s %s\n", value, label);
+	printf("           %s %s\n", value, label);
+}
+
+
+void emit0(char* value) {
+	fprintf(ucodeFile, "           %s\n", value);
+	printf("           %s\n", value);
+}
+
+void emit1(char* value, int p) {
+	fprintf(ucodeFile, "           %s %d\n", value, p);
+	printf("           %s %d\n", value, p);
+}
+
+void emit2(char* value, int p, int q) {
+	fprintf(ucodeFile, "           %s %d %d\n", value, p, q);
+	printf("           %s %d %d\n", value, p, q);
+}
+
+void emitFunc(char* value, int p, int q, int r) {
+	fprintf(ucodeFile, "           %s %d %d %d\n", value, p, q, r);
+	printf("           %s %d %d %d\n", value, p, q, r);
+}
+
+void genLabel(char* label) {
+	static int labelNum = 0;
+	//sprintf(label, "$$%d", labelNum++);
+	sprintf_s(label,sizeof(label) ,"$$%d", labelNum++);
+}
+
+void emitLabel(char* label) {
+	fprintf(ucodeFile, "%-10s nop\n", label);
+	printf("%-10s nop\n", label);
+}
+
+void icg_error(int err) {
+	printf("error %d\n", err);
+}
+
+void rv_emit(Node* ptr) {
+	int stIndex;
+
+	if (ptr->token.number == tnumber)
+		emit1("ldc", ptr->token.value.num);
+	else {
+		stIndex = lookup(ptr->token.value.id);
+
+		if (stIndex == -1) return;
+		if (symbolTable[stIndex].typeQualifier == CONST_TYPE) // constant
+			emit1("ldc", symbolTable[stIndex].initialValue);
+		else if (symbolTable[stIndex].width > 1) { 			// array var
+			emit2("lda", symbolTable[stIndex].base, symbolTable[stIndex].offset);
+		}
+		else 												// simple var
+			emit2("lod", symbolTable[stIndex].base, symbolTable[stIndex].offset);
+	}
+}
+
+
+/*------------------ TABLE INITIALIZE FUNCTION -----------------------------*/
+void initSymbolTable()
+{
+	int i;
+
+	symbolTableTop = -1;
+	levelTop = -1;
+	base = 1; offset = 1;
+
+	for (i = 0; i < HASH_BUCKET_SIZE; i++)
+		hashBucket[i] = -1;
+
+	for (i = 0; i < LEVEL_STACK_SIZE; i++)
+		levelStack[i] = 0;
+}
+
+/*--------------------- HASH FUNCTION --------------------------------------*/
+int hash(char* symbolName)
+{
+	int hashValue;
+
+	for (hashValue = 0; *symbolName; symbolName++)
+		hashValue += *symbolName;
+
+	return (hashValue % HASH_BUCKET_SIZE);
+}
+
+
+/*------------------ LOOKUP FUNCTION ---------------------------------------*/
+int lookup(char* symbol)
+{
+	int stIndex;
+
+	stIndex = hash(symbol);
+	for (stIndex = hashBucket[stIndex];
+		stIndex >= 0 && strcmp(symbol, symbolTable[stIndex].symbolName);
+		stIndex = symbolTable[stIndex].nextIndex);
+
+	if (stIndex < 0)
+		printf("undefined identifier: %s\n", symbol);
+
+	return stIndex;
+}
+
+/*-------------------- INSERT SYMBOL FUNCTION ------------------------------*/
+int insert(char* symbol, int specifier, int qualifier, int base, int offset,
+	int width, int initialValue)
+{
+	int hashValue;
+	int i;
+
+	hashValue = hash(symbol);
+	for (i = hashBucket[hashValue]; i >= 0 && strcmp(symbol, symbolTable[i].symbolName);)
+		i = symbolTable[i].nextIndex;
+
+	if ((i >= 0) && (base == symbolTable[i].base)) {	// found
+		printf("multiply defined identifier : %s\n", symbol);
+
+		printf("base = %d, symbolTable[i].base = %d\n", base, symbolTable[i].base);
+		return -1;
+	}
+
+	if (symbolTableTop > SYMTAB_SIZE) {
+		printf("symbol table overflow\n");
+		dumpSymbolTable();
+		exit(1);
+	}
+
+	symbolTableTop++;
+	//strcpy(symbolTable[symbolTableTop].symbolName, symbol);
+	strcpy_s(symbolTable[symbolTableTop].symbolName, sizeof(symbolTable[symbolTableTop].symbolName) ,symbol);
+	symbolTable[symbolTableTop].typeSpecifier = specifier;
+	symbolTable[symbolTableTop].typeQualifier = qualifier;
+	symbolTable[symbolTableTop].base = base;
+	symbolTable[symbolTableTop].offset = offset;
+	symbolTable[symbolTableTop].width = width;
+	symbolTable[symbolTableTop].initialValue = initialValue;
+
+	symbolTable[symbolTableTop].nextIndex = hashBucket[hashValue];
+	hashBucket[hashValue] = symbolTableTop;
+
+	return symbolTableTop;
+}
+
+
+/*------------------ SET BLOCK FUNCTION ------------------------------------*/
+void set()
+{
+	++levelTop;
+	//	printf("level stack top in set function = %d\n", levelTop);
+	if (levelTop > LEVEL_STACK_SIZE)
+		printf("error: level stack overflow.\n");
+
+	levelStack[levelTop] = symbolTableTop;
+	++base; offset = 1;
+}
+
+/*--------------------- RESET BLOCK FUNCTION -------------------------------*/
+void reset(void)
+{
+	int hashValue;
+	int i;
+
+	i = symbolTableTop;
+	symbolTableTop = levelStack[levelTop];
+	levelTop--;
+
+	for (; i > symbolTableTop; i--) {
+		hashValue = hash(symbolTable[i].symbolName);
+		hashBucket[hashValue] = symbolTable[i].nextIndex;
+	}
+	base--;
+
+}
+
+/*-------------------   Dump Symbol Table   -----------------------------*/
+void dumpSymbolTable()
+{
+	int i;
+
+	printf(" === Dump Symbol Table ===\n");
+	for (i = 0; i <= symbolTableTop; i++)
+		printf("id = %-12s : \ttype = %s, \tqualifier = %s, \tbase = %d, \toffset = %d \twidth = %d, \tvalue = %d\n",
+			symbolTable[i].symbolName,
+			typeName[symbolTable[i].typeSpecifier],
+			qualifierName[symbolTable[i].typeQualifier],
+			symbolTable[i].base,
+			symbolTable[i].offset,
+			symbolTable[i].width,
+			symbolTable[i].initialValue);
+}
+
+/*------------------- generate Symbol Table -----------------------------*/
+void genSym(int base)
+{
+	int stIndex;
+
+	//	//fprintf(ucodeFile, "// Information for Symbol Table\n");
+
+	for (stIndex = 0; stIndex <= symbolTableTop; stIndex++) {
+		if ((symbolTable[stIndex].typeQualifier == FUNC_TYPE) ||
+			(symbolTable[stIndex].typeQualifier == CONST_TYPE)) continue;
+
+		if (base == symbolTable[stIndex].base)
+			emitSym(symbolTable[stIndex].base, symbolTable[stIndex].offset, symbolTable[stIndex].width);
+	}
+}
---
+++ 컴파일러/Ucode_Translator/Ucode_Translator/scanner.c
@@ -0,0 +1,268 @@
+#pragma once
+#include "scanner.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+extern FILE* sourceFile;
+
+struct tokenType scanner() {
+	struct tokenType token;
+	int i, j, k, num;
+	char ch, id[ID_LENGTH];
+
+	token.number = tnull;
+
+	do {
+		while (isspace(ch = fgetc(sourceFile)));
+
+		if (isalpha(ch)) {
+			i = 0;
+			do {
+				if (i < ID_LENGTH)
+					id[i++] = ch;
+
+				ch = fgetc(sourceFile);
+			} while (isalnum(ch));
+			id[i] = '\0';
+
+			ungetc(ch, sourceFile);
+
+			for (i = 0; i < NUMKEYWORD; i++) {
+				if (!strcmp(id, keyword[i])) {
+					j = i;
+					break;
+				}
+				else
+					j = -1;
+			}
+
+
+			if (j != -1) {
+				token.number = tnum[j];
+				//strcpy(token.value.id, id);
+				strcpy_s(token.value.id,sizeof(token.value.id), id);
+			}
+			else {
+				token.number = tident;
+				//strcpy(token.value.id, id);
+				strcpy_s(token.value.id,sizeof(token.value.id), id);
+			}
+		}
+
+		else if (isdigit(ch)) {
+			//printf("this is in the isdigit");
+			num = 0;
+
+			do {
+				num = 10 * num + (int)(ch - '0');
+				ch = fgetc(sourceFile);
+			} while (isdigit(ch));
+
+			ungetc(ch, sourceFile);
+			token.number = tnumber;
+			token.value.num = num;
+		} // number
+
+		else {
+			switch (ch) {		//special characters
+			case '/':
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile); // state 10
+
+				if (ch == '*') {
+					do {
+						while (ch != '*')
+							ch = fgetc(sourceFile);
+
+						ch = fgetc(sourceFile);
+					} while (ch != '/');
+
+				}
+				else if (ch == '/') {
+					while (fgetc(sourceFile) != '\n');
+
+
+				}
+				else if (ch == '=') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tdivAssign;
+				}
+				else {
+					token.value.id[1] = '\0';
+					token.number = tdiv;
+					ungetc(ch, sourceFile);
+				} break;
+
+			case '!':
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile);
+				if (ch == '=') {		//state 
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tnotequ;
+				}
+				else {			//state 0
+					token.value.id[1] = '\0';
+					token.number = tnot;
+					ungetc(ch, sourceFile);
+				} break;
+
+			case '%':
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile);
+				if (ch == '=') {	//state 3
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tmodAssign;
+				}
+				else {		//state 2
+					token.value.id[1] = '\0';
+					token.number = tmod;
+					ungetc(ch, sourceFile);
+				} break;
+
+			case '<':				//state 11
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile);
+				if (ch == '=') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tlesse;
+				}
+				else {
+					token.value.id[1] = '\0';
+					token.number = tless;
+					ungetc(ch, sourceFile); //retract
+				} break;
+
+			case '>':				//state 15
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile);
+				if (ch == '=') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tgreate;
+				}
+				else {
+					token.value.id[1] = '\0';
+					token.number = tgreat;
+					ungetc(ch, sourceFile);
+				} break;
+
+			case '=':
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile);
+				if (ch == '=') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tequal;
+				}
+				else {
+					token.value.id[1] = '\0';
+					token.number = tassign;
+					ungetc(ch, sourceFile);
+				} break;
+			case '&':
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile);
+				if (ch == '&') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tand;
+				} break;
+
+			case '(':
+				token.value.id[0] = ch; token.value.id[1] = '\0';
+				token.number = tlparen;
+				break;
+			case ')':
+				token.value.id[0] = ch; token.value.id[1] = '\0';
+				token.number = trparen;
+				break;
+
+			case '+':
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile);
+				if (ch == '+') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tinc;
+				}
+				else if (ch == '=') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = taddAssign;
+				}
+				else {
+					token.value.id[1] = '\0';
+					token.number = tplus;
+					ungetc(ch, sourceFile);
+				} break;
+				break;
+
+			case '-':
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile);
+				if (ch == '-') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tdec;
+				}
+				else if (ch == '=') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tsubAssign;
+				}
+				else {
+					token.value.id[1] = '\0';
+					token.number = tminus;
+					ungetc(ch, sourceFile);
+				}
+				break;
+
+			case '*':
+				token.value.id[0] = ch;
+				ch = fgetc(sourceFile);
+				if (ch == '=') {
+					token.value.id[1] = ch; token.value.id[2] = '\0';
+					token.number = tmulAssign;
+				}
+				else {
+					token.value.id[1] = '\0';
+					token.number = tmul;
+					ungetc(ch, sourceFile);
+				}
+				break;
+				break;
+
+			case ';':
+				token.value.id[0] = ch; token.value.id[1] = '\0';
+				token.number = tsemicolon;
+				break;
+
+			case '{':
+				token.value.id[0] = ch; token.value.id[1] = '\0';
+				token.number = tlbrace;
+				break;
+
+			case '}':
+				token.value.id[0] = ch; token.value.id[1] = '\0';
+				token.number = trbrace;
+				break;
+			case ',':
+				token.value.id[0] = ch; token.value.id[1] = '\0';
+				token.number = tcomma;
+				break;
+			case '[':
+				token.value.id[0] = ch; token.value.id[1] = '\0';
+				token.number = tlbracket;
+				break;
+			case ']':
+				token.value.id[0] = ch; token.value.id[1] = '\0';
+				token.number = trbracket;
+				break;
+
+			case EOF:
+				token.number = teof;
+				break;
+			}	// switch end
+		}
+
+
+	} while (token.number == tnull);
+
+	return token;
+}
